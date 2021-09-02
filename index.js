c7633fde53ca1d588f2c9133477c7f1291e64c32
HardikAgrawal2010 <60972968+HardikAgrawal2010@users.noreply.github.com>
2021-09-02 11:51:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.loadPlugin(cmd)
 
 bot.on('login',function(){
 	console.log("Logged In")
-	bot.chat("hello");
+	bot.chat("/register hardik@2010 hardik@2010");
 });
 
 bot.on('time', function(time) {
